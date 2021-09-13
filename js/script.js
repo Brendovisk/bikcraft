83de57d429add77d1804dbcda9e41bfff5305865
Brendon Franco <brendon.franco@vibryt.com.br>
2021-09-12 21:26:19 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const btnMobile = document.getElementById('btn-mobile');
+const btnAnchor = document.getElementsByClassName('menu__item');
+var countBtnAnchor = btnAnchor.length;
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Close menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Open Menu');
+    }
+}
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+for (var i = 0; i < countBtnAnchor; i++) {
+    btnAnchor[i].addEventListener('click', toggleMenu);
+};
